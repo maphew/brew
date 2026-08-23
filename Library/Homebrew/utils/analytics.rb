d361df28f096e6e227cedfe687a57427a7c20bb8
Sean Molenaar <SMillerDev@users.noreply.github.com>
2026-08-23 14:33:18 +0200

    Update analytics.rb

--- Library/Homebrew/utils/analytics.rb
+++ Library/Homebrew/utils/analytics.rb
@@ -52,7 +52,7 @@ module Utils
         ]
 
         # Second precision is highest we can do and has the lowest performance cost.
-        url = "#{INFLUX_HOST}/api/v3/write_lp?bucket=#{INFLUX_BUCKET}&precision=seconds&no_sync=true"
+        url = "#{INFLUX_HOST}/api/v3/write_lp?db=#{INFLUX_BUCKET}&precision=second&no_sync=true"
         deferred_curl(url, args)
       end
 
